e7264c9251ddb5aee5e0cac46e7bf698aa786ab2
AimableHonorable <aimatwiri@gmail.com>
2021-08-02 13:12:16 +0200

    created a form to uupload the image and set error message when user tries to upload non-image files

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const UploadForm = () => {
+  const [file, setFile] = useState(null)
+  const [error, setError] = useState(null)
+
+  // declare allowed file types 
+  const types = ['image/jpeg', 'image/png']
+
+  const changeHandle = (e) => {
+    
+    let selected = e.target.files[0]
+
+    // check if we have the file and the file is the allowed type of file
+    if (selected && types.includes(selected.type)) {
+      setFile(selected)
+      setError(null) //reset the error message to null when there is user select a correct file
+    } else {
+      setFile(null)
+
+      // set error when the user tries to upload a file other than image file
+      setError('Please select an image file (png or jpeg)')
+    }
+  }
+  return (
+    <form>
+      <input type="file" onChange={changeHandle} />
+      <div className="output">
+        { error && <div className="error">{ error }</div> }
+        { file && <div>{ file.name }</div> }
+      </div>
+    </form>
+  )
+}
+
+export default UploadForm
